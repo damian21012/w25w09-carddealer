a09ea6e11f5ef1466162cf8a1b7d459a90975f0b
damian21012 <mlgk3441@naver.com>
2025-11-11 11:30:15 +0900

    🎉프로젝트 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20220809/w25w09_carddealer/W25w09CarddealerApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220809.w25w09_carddealer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W25w09CarddealerApplication
+
+fun main(args: Array<String>) {
+	runApplication<W25w09CarddealerApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20220809/w25w09_carddealer/W25w09CarddealerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220809.w25w09_carddealer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W25w09CarddealerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
